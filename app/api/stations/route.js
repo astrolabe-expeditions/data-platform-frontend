326b5744b54bb36f6c6fa65171b05448d09a3557
cballevre <celestin.ballevre@protonmail.com>
2023-07-28 23:04:16 +0200

    feat: Fetch data from server

---
+++ app/api/stations/route.js
@@ -0,0 +1,18 @@
+import { db } from "@/lib/db"
+
+
+export async function GET() {
+  try {
+    const stations = await db.station.findMany({
+      select: {
+        id: true,
+        name: true,
+      }
+    })
+
+    return new Response(JSON.stringify(stations))
+  } catch (error) {
+    console.log('error', error)
+    return new Response(null, { status: 500 })
+  }
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,19 @@
-export default function Home() {
+import { db } from "@/lib/db"
+
+export default async function Home() {
+
+  const stations = await db.station.findMany({
+    select: {
+      id: true,
+      name: true,
+    }
+  })
+
   return (
     <>
       <h2>Stations</h2>
       <ul>
-
+        {stations.map(({ id, name}) => <li key={id}>{name}</li>)}
       </ul>
     </>
     
