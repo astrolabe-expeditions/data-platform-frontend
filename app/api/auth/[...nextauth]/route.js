4aba5f1d73261a702f2cdc06ee2a733e69da6ab2
cballevre <celestin.ballevre@protonmail.com>
2023-07-30 18:42:19 +0200

    feat: Add prisma adapter for NextAuth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,11 @@
 import NextAuth from "next-auth"
-import GitHubProvider from "next-auth/providers/github";
+import GitHubProvider from "next-auth/providers/github"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 
-const authOptions = {
+import { db } from "@/lib/db"
+
+export const authOptions = {
+    adapter: PrismaAdapter(db),
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID,
