04d06d06053ffe6105fc495553c7c900ee71bc6c
cballevre <celestin.ballevre@protonmail.com>
2023-08-10 13:07:13 +0200

    feat: Add design to signup page

--- app/auth/signup/page.jsx
+++ app/auth/signup/page.jsx
@@ -1,18 +1,23 @@
-import { Button } from "@/components/ui/Button/Button";
+import NextLink from "next/link";
+
 import { Input } from "@/components/ui/Input/Input";
-import Link from "next/link";
+import { Button } from "@/components/ui/Button/Button";
+import { Link } from "@/components/ui/Link";
+import { Typography } from "@/components/ui/Typography";
 
 export default function Signup() {
     return (
-        <div>
-            <button>Continuer avec Github</button>
-            <hr />
-            <form>
+        <>
+            <Typography variant="title" alignCenter className="mb-3">Create an account</Typography>
+            <Typography variant="subtitle" alignCenter className="mb-8">To manage your expeditions</Typography>
+            <form className="flex flex-col gap-3 w-full">
+                <Input type="text" name="name" label="Name" />
                 <Input type="email" name="email" label="Email" />
                 <Input type="password" name="password" label="Mot de passe" />
-                <Button type="submit" label="S'inscrire"/>
+                <Button type="submit" label="S'inscrire" />
             </form>
-            <p>Déjà inscrit ? <Link href="/auth/login">Connectez-vous</Link></p>
-        </div>
+            <Typography variant="body2" color="textSecondary" className="mt-8" alignCenter>
+                Déjà inscrit ? <Link variant="body2" href="/auth/login" as={NextLink}>Connectez-vous</Link></Typography>
+        </>
     )
 }
