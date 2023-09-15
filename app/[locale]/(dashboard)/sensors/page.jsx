d783d020d6be3eed2d387cc2ad2229b9f7a3e5d6
cballevre <celestin.ballevre@protonmail.com>
2023-09-15 21:42:54 +0200

    feat: Add sensors to database schema

--- app/[locale]/(dashboard)/sensors/page.jsx
+++ app/[locale]/(dashboard)/sensors/page.jsx
@@ -1,10 +1,33 @@
+import { getServerSession } from 'next-auth/next'
+import { redirect } from 'next/navigation'
+
+import { db } from '@/lib/db'
+import { authOptions } from '@/lib/auth'
 import { Page } from '@/components/Page/Page'
 import { PageHeader } from '@/components/Page/PageHeader'
 
-export default function Sensors() {
+export default async function Sensors() {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    redirect('/auth/login')
+  }
+
+  const sensors = await db.sensor.findMany({
+    select: {
+      id: true,
+      identifier: true,
+    },
+  })
+
   return (
     <Page>
       <PageHeader title="Capteurs" />
+      <ul>
+        {sensors.map(({ id, identifier }) => (
+          <li key={id}>{identifier}</li>
+        ))}
+      </ul>
     </Page>
   )
 }
