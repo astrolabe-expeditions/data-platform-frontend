aed60bcca97049d988fb651a3fa3fc8ab3d933d0
Felipe LOBATO <felipe.lobato@imt-atlantique.net>
2023-09-28 08:40:12 +0200

    feat: Dynamic routes for stations based on id

---
+++ app/[locale]/(dashboard)/[id]/page.jsx
@@ -0,0 +1,20 @@
+import { getServerSession } from 'next-auth/next'
+import { authOptions } from '@/lib/auth'
+import { Page } from '@/components/Page/Page'
+import { PageHeader } from '@/components/Page/PageHeader'
+
+async function Home({ params }) {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    redirect('/auth/login')
+  }
+
+  return (
+    <Page>
+      <PageHeader title={params.id} />
+    </Page>
+  )
+}
+
+export default Home
