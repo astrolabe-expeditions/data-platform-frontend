36ec62500e1a410e36ce45a68d3cacc9dec4511e
Felipe LOBATO <felipe.lobato@imt-atlantique.net>
2023-09-28 08:42:36 +0200

    feat: CRUD configuration v1

--- app/api/stations/route.js
+++ app/api/stations/route.js
@@ -1,11 +1,14 @@
 import { db } from '@/lib/db'
+import { NextResponse } from 'next/server'
 
-export async function GET() {
+export async function GET(req) {
   try {
     const stations = await db.station.findMany({
       select: {
-        id: true,
+        sensors: true,
         name: true,
+        type: true,
+        id: true,
       },
     })
 
@@ -13,5 +16,24 @@ export async function GET() {
   } catch (error) {
     console.log('error', error)
     return new Response(null, { status: 500 })
+  }
+}
+
+export async function POST(request) {
+  try {
+    return NextResponse.json({ success: true })
+  } catch (err) {
+    console.log(err)
+    return NextResponse.json({ message: err, success: false })
+  }
+}
+
+export async function DELETE(request) {
+  try {
+    console.log(request)
+    return NextResponse.json({ success: true })
+  } catch (err) {
+    console.log(err)
+    return NextResponse.json({ message: err, success: false })
   }
 }
