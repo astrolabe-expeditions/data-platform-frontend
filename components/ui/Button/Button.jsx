754f6af9315290521fe022b1eaee25a2e463f77f
cballevre <celestin.ballevre@protonmail.com>
2023-10-01 14:30:36 +0200

    feat(Button): Add ghost and link variant

--- components/ui/Button/Button.jsx
+++ components/ui/Button/Button.jsx
@@ -4,23 +4,40 @@ import { ButtonBase } from './ButtonBase'
 
 const Button = ({
   label,
+  variant = 'solid',
   size = 'md',
   className,
   leftIcon,
   rightIcon,
   ...props
 }) => {
-  const sizes = {
-    sm: 'py-2 px-3.5 text-sm gap-1.5',
-    md: 'py-2.5 px-4 text-sm gap-1.5',
-    lg: 'py-2.5 px-4 text-base gap-2',
-    xl: 'py-3 px-5 text-base gap-2',
-    '2xl': 'py-4 px-7 text-lg gap-2.5',
+  const spacings = {
+    sm: 'py-2 px-3.5',
+    md: 'py-2.5 px-4',
+    lg: 'py-2.5 px-4',
+    xl: 'py-3 px-5',
+    '2xl': 'py-4 px-7',
   }
+
+  const sizes = {
+    sm: 'text-sm gap-1.5',
+    md: 'text-sm gap-1.5',
+    lg: 'text-base gap-2',
+    xl: 'text-base gap-2',
+    '2xl': 'text-lg gap-2.5',
+  }
+
+  const hasSpacing = variant !== 'link'
 
   return (
     <ButtonBase
-      className={clsx(className, 'flex items-center', sizes[size])}
+      className={clsx(
+        className,
+        'flex items-center',
+        sizes[size],
+        hasSpacing ? spacings[size] : null,
+      )}
+      variant={variant}
       {...props}>
       {leftIcon ? <Icon icon={leftIcon} /> : null}
       {label}
