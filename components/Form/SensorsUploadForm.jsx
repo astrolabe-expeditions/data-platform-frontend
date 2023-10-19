a4cd08f8203660bcd5c6a96ff3890b8e4c7f666a
Felipe LOBATO <felipelobato99@poli.ufrj.br>
2023-10-19 22:08:27 +0200

    feat: upload component for sensor

---
+++ components/Form/SensorsUploadForm.jsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { useTranslations as getTranslations } from 'next-intl'
+import { useState } from 'react'
+import { useRef } from 'react'
+import { Button } from '@/components/ui/Button/Button'
+import { Typography } from '@/components/ui/Typography'
+import { PageHeader } from '@/components/Page/PageHeader'
+import { SensorCard } from '@/components/Card/SensorCard'
+
+export default function SensorsUploadForm({ sensor }) {
+  const t = getTranslations('Sensors')
+
+  const hiddenFileInput = useRef(null)
+  const [formData, setFormData] = useState({
+    file: null,
+  })
+
+  const handleInputChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      const i = event.target.files[0]
+      setFormData({ ...formData, file: i })
+    }
+  }
+
+  const handleClick = (event) => {
+    hiddenFileInput.current.click()
+  }
+
+  return (
+    <>
+      <PageHeader title={t('upload_screen.upload_file')} />
+      <SensorCard sensor={sensor} />
+      <input
+        type="file"
+        ref={hiddenFileInput}
+        onChange={handleInputChange}
+        style={{ display: 'none' }}
+      />
+
+      <Button
+        onClick={handleClick}
+        label={t('upload_screen.upload_file_for_sensor')}
+      />
+      {formData.file !== null && (
+        <div>
+          <Typography>{formData.file.name}</Typography>
+          <Typography>{formData.file.size}</Typography>
+        </div>
+      )}
+    </>
+  )
+}
