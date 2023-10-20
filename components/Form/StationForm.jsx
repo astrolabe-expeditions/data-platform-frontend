813a5205aa0b2c59b2bee59f2107c4dec8165eeb
ArthurNérambourg <arthur.nerambourg64@gmail.com>
2023-10-20 10:17:49 +0200

    Station Form Input

--- components/Form/StationForm.jsx
+++ components/Form/StationForm.jsx
@@ -1,49 +1,117 @@
 'use client'
 import React, { useState } from 'react'
-import { Input } from '../ui/Input/Input'
+import { Input } from '@/components/ui/Input/Input'
 import { Select } from '../ui/Select/Select'
 import { Button } from '../ui/Button/Button'
+import { useTranslations as getTranslations } from 'next-intl'
 
-const StationForm = ({ columns }) => {
+export default function StationForm() {
+
+  const t = getTranslations('Station')
+
+  const [formData, setFormData] = useState({
+    name: '',
+    type: '',
+    sensors: [],
+    longitude: '',
+    latitude: '',
+    description: '',
+    image_URL: '',
+  })
+
+  const handleChange = (inputName) => (evt) => {
+    setFormData({
+      ...formData,
+      [inputName]: evt.target.value,
+    })
+  }
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    signIn('credentials', {
+      ...formData,
+      callbackUrl: '/',
+    })
+  }
+
   return (
-    <form className="w-full max-w-lg">
-      <div className="flex flex-wrap -mx-3 mb-6">
-        <div className="w-full px-3">
-          <Input label={'Name'}></Input>
+    <>
+      
+      <form className="w-full max-w-lg" onSubmit={handleSubmit}>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <Input 
+              name = "name"
+              label={t('labels.name')}
+              value={formData.name}
+              onChange={handleChange('name')}
+            />
+          </div>
         </div>
-      </div>
-      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="flex flex-wrap -mx-3 mb-6">
         <div className="w-full px-3">
-          <Select label={'Type'}></Select>
+          <Select
+            name="type"
+            label={t('labels.type')}
+            value={formData.type}
+            onChange={handleChange('type')}
+          />
+          </div>
         </div>
-      </div>
-      <div className="flex flex-wrap -mx-3 mb-6">
-        <div className="w-full px-3">
-          <Select label={'Sensors'}></Select>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <Select
+              name="sensors"
+              label={t('labels.sensors')}
+              value={formData.sensors}
+              onChange={handleChange('sensors')}
+            />
+          </div>
         </div>
-      </div>
-      <div className="flex flex-wrap -mx-3 mb-6">
-        <div className="w-full px-3">
-          <Input label={'Latitude'}></Input>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <Input
+              name="latitude"
+              label={t('labels.latitude')}
+              value={formData.latitude}
+              onChange={handleChange('latitude')}
+            />
+          </div>
         </div>
-      </div>
-      <div className="flex flex-wrap -mx-3 mb-6">
-        <div className="w-full px-3">
-          <Input label={'Longitude'}></Input>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <Input
+              name="longitude"
+              label={t('labels.longitude')}
+              value={formData.longitude}
+              onChange={handleChange('longitude')}
+            />
+          </div>
         </div>
-      </div>
-      <div className="flex flex-wrap -mx-3 mb-6">
-        <div className="w-full px-3">
-          <Input label={'Description'}></Input>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <Input
+              name="description"
+              label={t('labels.description')}
+              value={formData.description}
+              onChange={handleChange('description')}
+            />
+          </div>
         </div>
-      </div>
-      <div className="flex flex-wrap -mx-3 mb-6">
-        <div className="w-full px-3">
-          <Input label={'Image_URL'}></Input>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <Input
+              name="image_URL"
+              label={t('labels.image_URL')}
+              value={formData.image_URL}
+              onChange={handleChange('image_URL')}
+            />
+          </div>
         </div>
-      </div>
-      <Button label={'Add station'} />
-    </form>
+        <Button label={'Add station'} />
+      </form>
+    </>
+
   )
 }
 
