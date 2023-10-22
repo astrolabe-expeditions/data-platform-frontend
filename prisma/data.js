705831eabd93aa85d5e6dc97a7340463a9de9c78
Mohamed Taher MAALEJ <mohamedtahermaalej@gmail.com>
2023-10-22 03:01:26 +0100

    feat: Add new data to the database

--- prisma/data.js
+++ prisma/data.js
@@ -78,20 +78,6 @@ const sensors = [
 
 const records = [
   {
-    latitude: '40.7128',
-    longitude: '-74.0060',
-    recorded_at: new Date(),
-    battery_voltage: 3.7,
-    battery_percentage: 90,
-    pression_ext: 1013.25,
-    temp_ext: 25.5,
-    temp_int: 21.3,
-    temp_sea: [18.5, 19.0, 19.2],
-    temp_sea_mean: 18.9,
-    ec_sea: [35.5, 35.7, 35.9],
-    ec_sea_mean: 35.7,
-  },
-  {
     latitude: '34.0522',
     longitude: '-118.2437',
     recorded_at: new Date(),
@@ -106,8 +92,8 @@ const records = [
     ec_sea_mean: 36.1,
   },
   {
-    latitude: '41.8781',
-    longitude: '-87.6298',
+    latitude: '48.8566',
+    longitude: '2.3522',
     recorded_at: new Date(),
     battery_voltage: 3.6,
     battery_percentage: 87,
@@ -118,34 +104,6 @@ const records = [
     temp_sea_mean: 20.7,
     ec_sea: [36.2, 36.3, 36.1],
     ec_sea_mean: 36.2,
-  },
-  {
-    latitude: '51.5074',
-    longitude: '0.1278',
-    recorded_at: new Date(),
-    battery_voltage: 3.8,
-    battery_percentage: 91,
-    pression_ext: 1014.28,
-    temp_ext: 23.5,
-    temp_int: 20.1,
-    temp_sea: [18.7, 18.9, 18.6],
-    temp_sea_mean: 18.7,
-    ec_sea: [35.8, 35.9, 35.7],
-    ec_sea_mean: 35.8,
-  },
-  {
-    latitude: '52.5200',
-    longitude: '13.4050',
-    recorded_at: new Date(),
-    battery_voltage: 3.5,
-    battery_percentage: 88,
-    pression_ext: 1010.98,
-    temp_ext: 24.3,
-    temp_int: 21.6,
-    temp_sea: [19.8, 19.9, 19.7],
-    temp_sea_mean: 19.8,
-    ec_sea: [35.7, 35.8, 35.6],
-    ec_sea_mean: 35.7,
   },
 ]
 
@@ -180,24 +138,6 @@ const files = [
     status: 'Inactive',
     file_url: 'https://example.com/file5.csv',
   },
-  {
-    name: 'File 6',
-    update_date: new Date(),
-    status: 'Active',
-    file_url: 'https://example.com/file6.csv',
-  },
-  {
-    name: 'File 7',
-    update_date: new Date(),
-    status: 'Active',
-    file_url: 'https://example.com/file7.csv',
-  },
-  {
-    name: 'File 8',
-    update_date: new Date(),
-    status: 'Inactive',
-    file_url: 'https://example.com/file8.csv',
-  },
 ]
 
 const organisations = [
@@ -205,11 +145,6 @@ const organisations = [
     name: 'Environmental Research Institute',
     description:
       'A leading institute dedicated to researching and addressing environmental challenges.',
-  },
-  {
-    name: 'Tech Innovators for Sustainability',
-    description:
-      'A tech organization focused on sustainable solutions and environmental data analysis.',
   },
 ]
 
--- prisma/seed.js
+++ prisma/seed.js
@@ -68,7 +68,39 @@ const load = async () => {
         image_url: 'https://example.com/station6.jpg',
         sensors: {
           create: [
-            { identifier: 'UV987654', type: 'SensOcean', nbr_measures: 720 },
+            {
+              identifier: 'UV987654',
+              type: 'SensOcean',
+              nbr_measures: 720,
+              records: {
+                create: [
+                  {
+                    latitude: '52.5200',
+                    longitude: '13.4050',
+                    recorded_at: new Date(),
+                    battery_voltage: 3.5,
+                    battery_percentage: 88,
+                    pression_ext: 1010.98,
+                    temp_ext: 24.3,
+                    temp_int: 21.6,
+                    temp_sea: [19.8, 19.9, 19.7],
+                    temp_sea_mean: 19.8,
+                    ec_sea: [35.7, 35.8, 35.6],
+                    ec_sea_mean: 35.7,
+                  },
+                ],
+              },
+              files: {
+                create: [
+                  {
+                    name: 'File 6',
+                    update_date: new Date(),
+                    status: 'Active',
+                    file_url: 'https://example.com/file6.csv',
+                  },
+                ],
+              },
+            },
           ],
         },
       },
@@ -84,7 +116,39 @@ const load = async () => {
         image_url: 'https://example.com/station7.jpg',
         sensors: {
           create: [
-            { identifier: 'QR123456', type: 'LittObs', nbr_measures: 920 },
+            {
+              identifier: 'QR123456',
+              type: 'LittObs',
+              nbr_measures: 920,
+              records: {
+                create: [
+                  {
+                    latitude: '51.5074',
+                    longitude: '0.1278',
+                    recorded_at: new Date(),
+                    battery_voltage: 3.8,
+                    battery_percentage: 91,
+                    pression_ext: 1014.28,
+                    temp_ext: 23.5,
+                    temp_int: 20.1,
+                    temp_sea: [18.7, 18.9, 18.6],
+                    temp_sea_mean: 18.7,
+                    ec_sea: [35.8, 35.9, 35.7],
+                    ec_sea_mean: 35.8,
+                  },
+                ],
+              },
+              files: {
+                create: [
+                  {
+                    name: 'File 7',
+                    update_date: new Date(),
+                    status: 'Inactive',
+                    file_url: 'https://example.com/file7.csv',
+                  },
+                ],
+              },
+            },
           ],
         },
       },
@@ -100,7 +164,39 @@ const load = async () => {
         image_url: 'https://example.com/station8.jpg',
         sensors: {
           create: [
-            { identifier: 'AB123456', type: 'LittObs', nbr_measures: 800 },
+            {
+              identifier: 'AB123456',
+              type: 'LittObs',
+              nbr_measures: 800,
+              records: {
+                create: [
+                  {
+                    latitude: '41.8781',
+                    longitude: '-87.6298',
+                    recorded_at: new Date(),
+                    battery_voltage: 3.6,
+                    battery_percentage: 87,
+                    pression_ext: 1011.75,
+                    temp_ext: 26.8,
+                    temp_int: 23.2,
+                    temp_sea: [20.5, 20.7, 20.8],
+                    temp_sea_mean: 20.7,
+                    ec_sea: [36.2, 36.3, 36.1],
+                    ec_sea_mean: 36.2,
+                  },
+                ],
+              },
+              files: {
+                create: [
+                  {
+                    name: 'File 8',
+                    update_date: new Date(),
+                    status: 'Inactive',
+                    file_url: 'https://example.com/file8.csv',
+                  },
+                ],
+              },
+            },
           ],
         },
       },
@@ -116,7 +212,39 @@ const load = async () => {
         image_url: 'https://example.com/station9.jpg',
         sensors: {
           create: [
-            { identifier: 'OP234567', type: 'LittObs', nbr_measures: 780 },
+            {
+              identifier: 'OP234567',
+              type: 'LittObs',
+              nbr_measures: 780,
+              records: {
+                create: [
+                  {
+                    latitude: '34.0522',
+                    longitude: '-118.2437',
+                    recorded_at: new Date(),
+                    battery_voltage: 3.6,
+                    battery_percentage: 75,
+                    pression_ext: 1015.75,
+                    temp_ext: 28.2,
+                    temp_int: 22.8,
+                    temp_sea: [20.0, 20.2, 20.1],
+                    temp_sea_mean: 20.1,
+                    ec_sea: [36.1, 36.0, 36.2],
+                    ec_sea_mean: 36.1,
+                  },
+                ],
+              },
+              files: {
+                create: [
+                  {
+                    name: 'File 9',
+                    update_date: new Date(),
+                    status: 'Inactive',
+                    file_url: 'https://example.com/file9.csv',
+                  },
+                ],
+              },
+            },
           ],
         },
       },
@@ -132,7 +260,39 @@ const load = async () => {
         image_url: 'https://example.com/station10.jpg',
         sensors: {
           create: [
-            { identifier: 'MN567890', type: 'SensOcean', nbr_measures: 630 },
+            {
+              identifier: 'MN567890',
+              type: 'SensOcean',
+              nbr_measures: 630,
+              records: {
+                create: [
+                  {
+                    latitude: '40.7128',
+                    longitude: '-74.0060',
+                    recorded_at: new Date(),
+                    battery_voltage: 3.7,
+                    battery_percentage: 90,
+                    pression_ext: 1013.25,
+                    temp_ext: 25.5,
+                    temp_int: 21.3,
+                    temp_sea: [18.5, 19.0, 19.2],
+                    temp_sea_mean: 18.9,
+                    ec_sea: [35.5, 35.7, 35.9],
+                    ec_sea_mean: 35.7,
+                  },
+                ],
+              },
+              files: {
+                create: [
+                  {
+                    name: 'File 10',
+                    update_date: new Date(),
+                    status: 'Inactive',
+                    file_url: 'https://example.com/file10.csv',
+                  },
+                ],
+              },
+            },
           ],
         },
       },
@@ -146,8 +306,8 @@ const load = async () => {
     })
 
     console.log('Added user data')
+
     const password = await hash('hi123', 12)
-    console.log(password)
 
     const organisation_3 = await prisma.organisation.create({
       data: {
