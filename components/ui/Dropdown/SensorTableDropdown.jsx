6027627583b67cf0ca89696c054953b5d74ae2b0
Mohamed Taher MAALEJ <mohamedtahermaalej@gmail.com>
2023-11-27 00:40:29 +0100

    feat: Add Sensor Table Dropdown with Edit and Delete

---
+++ components/ui/Dropdown/SensorTableDropdown.jsx
@@ -0,0 +1,48 @@
+import { DropdownMenu, Button, IconButton, Dialog } from '@radix-ui/themes'
+import { EllipsisVertical } from '../Icons'
+import { Link } from '../Link'
+
+import { useRouter } from 'next/navigation'
+
+const SensorTableDropdown = ({ obj, ...props }) => {
+  const router = useRouter()
+  const removeSensor = async () => {
+    const confirmed = confirm('Are you sure you want to delete this Sensor?')
+
+    if (confirmed) {
+      const res = await fetch(
+        `http://localhost:3000/api/sensors?id=${obj.id}`,
+        {
+          method: 'DELETE',
+        },
+      )
+      if (res.ok) {
+        router.refresh()
+      }
+    }
+  }
+
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <IconButton variant="soft" size={'2'}>
+          <EllipsisVertical
+            width="20"
+            height="20"
+            color={'gray'}></EllipsisVertical>
+        </IconButton>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content>
+        <Link href={`/sensors/${obj.id}/edit`}>
+          <DropdownMenu.Item>Edit</DropdownMenu.Item>
+        </Link>
+        <DropdownMenu.Separator />
+        <DropdownMenu.Item color="red" onClick={removeSensor}>
+          Delete
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  )
+}
+
+export { SensorTableDropdown }
