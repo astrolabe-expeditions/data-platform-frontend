8570a9c36449452c9b4b132e57cacf3b27350f9a
cballevre <celestin.ballevre@protonmail.com>
2024-02-02 08:51:09 +0100

    refactor: Extract file upload into its own route

--- app/api/sensors/[id]/route.js
+++ app/api/sensors/[id]/route.js
@@ -1,10 +1,5 @@
 import { db } from '@/lib/db'
 import { NextResponse } from 'next/server'
-import {
-  createKeyInDatabase,
-  uploadFile,
-  sendMessageToProcessingQueue,
-} from '@/lib/aws'
 
 export async function PUT(request, { params }) {
   const { id } = params
@@ -52,38 +47,6 @@ export async function GET(request, { params }) {
     },
   })
   return NextResponse.json({ sensor }, { status: 200 })
-}
-
-export async function POST(request) {
-  const data = await request.formData()
-  const file = data.get('file')
-  const sensor_id = data.get('sensor_id')
-
-  if (!file) {
-    return NextResponse.json({ success: false })
-  }
-
-  const file_id = await createKeyInDatabase(file, sensor_id)
-  await uploadFile(file, file_id, sensor_id)
-
-  sendMessageToProcessingQueue(file_id)
-
-  const sensor = await db.sensor.findUnique({
-    where: {
-      id: sensor_id,
-    },
-    select: {
-      id: true,
-      identifier: true,
-      type: true,
-      nbr_measures: true,
-      station_id: true,
-      records: true,
-      files: true,
-    },
-  })
-
-  return NextResponse.json({ success: true, data: sensor }, { status: 201 })
 }
 
 /**
