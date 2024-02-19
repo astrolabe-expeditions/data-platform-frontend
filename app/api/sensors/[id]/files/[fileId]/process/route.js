a119695ab94b65e5f9915dceedb3979d13a0ee58
cballevre <celestin.ballevre@protonmail.com>
2024-02-19 19:35:48 +0100

    refactor: Regoup all routes to files api

---
+++ app/api/sensors/[id]/files/[fileId]/process/route.js
@@ -0,0 +1,33 @@
+import { NextResponse } from 'next/server'
+
+import { sendMessageToProcessingQueue } from '@/lib/aws'
+
+/**
+ * @swagger
+ * /api/sensors/{sensorId}/files/{fileId}/process:
+ *   get:
+ *     description: Process a file
+ *     tags:
+ *       - sensors
+ *     parameters:
+ *      - in: path
+ *        name: sensorId
+ *        schema:
+ *          type: string
+ *      - in: path
+ *        name: fileId
+ *        schema:
+ *          type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ *       404:
+ *         description: Not Found
+ */
+export async function GET(request, { params }) {
+  const { fileId } = params
+
+  sendMessageToProcessingQueue(fileId)
+
+  return NextResponse.json({ message: 'Success' }, { status: 200 })
+}
