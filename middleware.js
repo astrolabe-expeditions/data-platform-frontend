6fe160cf5d3a580d04dd2d3a3bb9fc1a09034e60
cballevre <celestin.ballevre@protonmail.com>
2024-02-20 07:50:00 +0100

    feat: Protect every route below admin

--- middleware.js
+++ middleware.js
@@ -1,10 +1,47 @@
-import createMiddleware from 'next-intl/middleware'
+import { withAuth } from 'next-auth/middleware'
+import createIntlMiddleware from 'next-intl/middleware'
 
-export default createMiddleware({
-  locales: ['en', 'fr'],
+const locales = ['en', 'fr']
+const publicPages = ['/', '/auth/login', '/auth/signup']
+
+const intlMiddleware = createIntlMiddleware({
+  locales,
   defaultLocale: 'en',
   localePrefix: 'never',
 })
+
+const authMiddleware = withAuth(
+  // Note that this callback is only invoked if
+  // the `authorized` callback has returned `true`
+  // and not for pages listed in `pages`.
+  function onSuccess(req) {
+    return intlMiddleware(req)
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => token != null,
+    },
+    pages: {
+      signIn: '/auth/login',
+    },
+  },
+)
+
+export default function middleware(req) {
+  const publicPathnameRegex = RegExp(
+    `^(/(${locales.join('|')}))?(${publicPages
+      .flatMap((p) => (p === '/' ? ['', '/'] : p))
+      .join('|')})/?$`,
+    'i',
+  )
+  const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname)
+
+  if (isPublicPage) {
+    return intlMiddleware(req)
+  } else {
+    return authMiddleware(req)
+  }
+}
 
 export const config = {
   matcher: ['/((?!api|_next|.*\\..*).*)'],
