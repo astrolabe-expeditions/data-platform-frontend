735d29cceafd337d13c6cf37e999efb91e33e054
cballevre <celestin.ballevre@protonmail.com>
2024-03-06 21:47:50 +0100

    feat(Sidebar): Improve translation

--- components/Sidebar/SidebarItem.jsx
+++ components/Sidebar/SidebarItem.jsx
@@ -4,9 +4,11 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import clsx from 'clsx'
 
-const SidebarItem = ({ name, to, onItemClick }) => {
+const SidebarItem = ({ name, to, onItemClick, exact }) => {
   const base = 'rounded-lg px-3 py-2 text-base hover:bg-primary-600'
   const pathname = usePathname()
+
+  const isActive = exact ? pathname === to : pathname.startsWith(to)
 
   return (
     <li onClick={onItemClick}>
@@ -14,7 +16,7 @@ const SidebarItem = ({ name, to, onItemClick }) => {
         <div
           className={clsx(
             base,
-            pathname === to ? 'bg-primary-600 text-white' : 'text-primary-100',
+            isActive ? 'bg-primary-600 text-white' : 'text-primary-100',
           )}>
           {name}
         </div>
