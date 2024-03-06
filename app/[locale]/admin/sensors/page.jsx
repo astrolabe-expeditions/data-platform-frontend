49de71a33d32e066aba3358960de0b0a744c7d2f
cballevre <celestin.ballevre@protonmail.com>
2024-03-06 08:49:14 +0100

    fix: Update List with new entry

--- app/[locale]/admin/sensors/page.jsx
+++ app/[locale]/admin/sensors/page.jsx
@@ -4,21 +4,21 @@ import SensorSearchBar from '@/components/Searchbar/SensorSearchBar'
 
 import { db } from '@/lib/db'
 
-const sensors = await db.sensor.findMany({
-  where: {
-    deleted: false,
-  },
-  select: {
-    id: true,
-    identifier: true,
-    type: true,
-    nbr_measures: true,
-    created_at: true,
-    updated_at: true,
-  },
-})
-
 async function SensorList() {
+  const sensors = await db.sensor.findMany({
+    where: {
+      deleted: false,
+    },
+    select: {
+      id: true,
+      identifier: true,
+      type: true,
+      nbr_measures: true,
+      created_at: true,
+      updated_at: true,
+    },
+  })
+
   return (
     <Page>
       <SensorSearchBar data={sensors} />
--- app/[locale]/admin/stations/page.jsx
+++ app/[locale]/admin/stations/page.jsx
@@ -6,19 +6,19 @@ import { Page } from '@/components/Page/Page'
 import StationSearchBar from '@/components/Searchbar/StationSearchBar'
 import { db } from '@/lib/db'
 
-const stations = await db.station.findMany({
-  where: {
-    deleted: false,
-  },
-  select: {
-    sensors: true,
-    name: true,
-    type: true,
-    id: true,
-  },
-})
-
 async function StationList() {
+  const stations = await db.station.findMany({
+    where: {
+      deleted: false,
+    },
+    select: {
+      sensors: true,
+      name: true,
+      type: true,
+      id: true,
+    },
+  })
+
   return (
     <Page>
       <StationSearchBar data={stations} />
