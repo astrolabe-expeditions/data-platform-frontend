846afe2486e6bceb45735994705effe13844390a
cballevre <celestin.ballevre@protonmail.com>
2024-03-12 08:49:50 +0100

    feat: Add route to upload csv by stranger

--- app/[locale]/(public)/page.jsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <div>
-      <h1>Astrolabe Expeditions</h1>
-    </div>
-  )
-}
-
-export default Home
---
+++ app/[locale]/(public)/upload/page.jsx
@@ -0,0 +1,9 @@
+function Upload() {
+  return (
+    <div>
+      <h1>Upload a new file</h1>
+    </div>
+  )
+}
+
+export default Upload
